c39209025629345ee80cb81c748ae313cbdaa537
Alec Livaditis <aleclivaditis@gmail.com>
2023-08-24 18:10:58 -0400

    feat graphql boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import './App.css';
 
 function App() {
+
+
+  //  an example of a get query
+  const GET_POST = gql`
+    query {
+      post(id: 1) {
+        id
+        title
+        body
+      }
+    }`;
+
+  const {loading, error, data} = useQuery(GET_POST)
+  {!loading && console.log(data)}
+
+  // an example of a post
+  const CREATE_POST = gql`
+    mutation CreatePost($input: CreatePostInput!) {
+      createPost(input: $input) {
+        id
+        title
+        body
+      }
+    }`;
+
+  const createPostInput = {
+    title: "A Very Captivating Post Title",
+    body: "Some interesting content."
+  };
+
+  const [createPost, { loading: mutationLoading, error: mutationError, data: mutationData }] = useMutation(CREATE_POST);
+
+  const handleCreatePost = () => {
+    createPost({
+      variables: {
+        input: createPostInput,
+      },
+    })
+    .then(res => console.log('post response:', res))
+  };  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading && 
+        <>
+          <h1>{data.post.title}</h1>
+          <p>{data.post.body}</p>
+          <button onClick={handleCreatePost}>
+            create post
+          </button>
+        </>
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+  // uri is going to change
+  uri: 'https://graphqlzero.almansi.me/api',
+  cache: new InMemoryCache(),
+});
+
+
 root.render(
+<ApolloProvider client={client}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+</ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
