df632e8a0d2250131b539e46a0e3d7178f13f1c8
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-25 14:45:40 -0700

    Import input.css to Landing and App components and style Landing background

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { GET_ALL_ITEMS } from '../api';
+import Landing from './Landing';
+import '../styles/input.css';
 
 function App() {
   const { loading, error, data } = useQuery(GET_ALL_ITEMS)
@@ -8,22 +10,22 @@ function App() {
 
   if (!loading) {
     console.log('data:', data)
-    allItems = data.products.map(item => {
-      return <div>
-        <h1>{item.name}</h1>
-        <p>{item.description}</p>
-        <p>{item.color}</p>
-        <p>{item.price}</p>
-      </div>
-    })
+    // allItems = data.products.map(item => {
+    //   return <div>
+    //     <h1>{item.name}</h1>
+    //     <p>{item.description}</p>
+    //     <p>{item.color}</p>
+    //     <p>{item.price}</p>
+    //   </div>
+    // })
   }
 
   return (
     <div className="App">
       {!loading && 
         <>
-          <h1>Remove this line once writing code.</h1>
-          {!error && allItems}
+          <Landing />
+          {/* {!error && allItems} */}
         </>
       }
     </div>
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import '../styles/input.css'
+
+export default function Landing() {
+  return (
+    <div className="landing">
+      <div className="landing__header">
+        <h1 className="landing__text">FROM PEAK TO POCKET</h1>
+      </div>
+    </div>
+  );
+
+}
