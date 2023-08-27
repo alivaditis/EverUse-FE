46e87552e3e6831853acb8ea4eda483ca53868ca
Jamie Caudill <j.caudill7177@gmail.com>
2023-08-27 13:54:19 -0700

    Move itemsForDisplay function to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,42 @@ import '../styles/input.css';
 
 function App() {
   const [items, setItems] = useState([])
+  const [itemsForDisplay, setItemsForDisplay] = useState([])
+
+  const getItemsForDisplay = () => {
+    let productsForDisplay = [];
+    
+    const productNames = new Set (items.map(product => product.name))
+    
+    productNames.forEach(productName => {
+      
+      const filteredProducts = items
+        .filter(product => product.name === productName)
+      
+      const filteredProduct = filteredProducts.reduce((acc, curr) => {
+        acc.id = curr.name;
+        acc.name = curr.name;
+        acc.price = curr.price;
+        acc.category = curr.category
+        acc.image = curr.image
+        acc.description = curr.description
+        if (!acc.colorOptions.includes(curr.color)) {
+          acc.colorOptions.push(curr.color)
+        }
+        if (!acc.sizeOptions.includes(curr.size)) {
+          acc.sizeOptions.push(curr.size)
+        }
+        return acc;
+      }, {
+        colorOptions: [],
+        sizeOptions: []
+      })
+      productsForDisplay.push(filteredProduct)
+    }) 
+    return productsForDisplay
+  }
+
+
   
   const { loading, error, data } = useQuery(GET_ALL_ITEMS)
   console.log(data)
@@ -16,6 +52,11 @@ function App() {
       setItems(data.products)
     }
   }, [data]);
+
+  useEffect(() => {
+    const displayProducts = getItemsForDisplay()
+    setItemsForDisplay(displayProducts)
+  }, [items]);
   
 
 
@@ -24,7 +65,7 @@ function App() {
       {!loading && 
         <>
           <Landing />
-          <Products items={items} />
+          <Products itemsForDisplay={itemsForDisplay} />
         </>
       }
     </div>
