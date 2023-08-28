2541d5642c2403cecb407e5a00507d124322d01c
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-28 19:59:08 -0400

    Locate the product from url

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -4,7 +4,8 @@ import { camelToPascalCase } from "../helperFunctions";
 const ProductDetail = ({itemsForDisplay}) => {
   console.log(itemsForDisplay)
   const productID = useParams().productID;
-  const product = itemsForDisplay.find(item => item.id === productID)
+  const product = itemsForDisplay.find(item => item.id === camelToPascalCase(productID));
+  console.log(product)
  
   return (
     <h1> test</h1>
