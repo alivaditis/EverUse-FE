a92d8f535a871d93b6ccb1466cff7c97070e662e
Rachel Prather <rachelprather1@gmail.com>
2023-08-28 14:29:34 -0700

    Remove unused useState

--- src/components/ShoppingBag.js
+++ src/components/ShoppingBag.js
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 const ShoppingBag = ({ shoppingBag, totalPrice, addTotalPrice, removeItemFromBag, updateQuantity }) =>{
   useEffect(() => {
