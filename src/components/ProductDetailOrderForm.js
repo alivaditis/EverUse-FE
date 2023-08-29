2b1918e1fc9f115f82b18f4f0c152f4bd3640cc5
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-29 01:44:36 -0400

    Add styling to form elements

--- src/components/ProductDetailOrderForm.js
+++ src/components/ProductDetailOrderForm.js
@@ -12,12 +12,18 @@ const ProductDetailOrderForm = ({product}) => {
          <p className="selection-text">Size:</p> 
          <div className="size-options-container">
           {!isSingleSize && <div className="multiple-choice-container">
-            <input type="radio" name="size-options" id="S" value="small" className="multiple-choice-option"/>
-            <label htmlFor='S'>S</label>
-            <input type="radio" name="size-options" id="M" value="medium" className="multiple-choice-option"/>
-            <label htmlFor='M'>M</label>
-            <input type="radio" name="size-options" id="S" value="large" className="multiple-choice-option"/>
-            <label htmlFor='L'>L</label>
+            <>
+              <input type="radio" name="size-options" id="S" value="small" className="multiple-choice-option"/>
+              <label htmlFor='S'>S</label>
+            </>
+            <>
+              <input type="radio" name="size-options" id="M" value="medium" className="multiple-choice-option"/>
+              <label htmlFor='M'>M</label>
+            </>
+            <>
+              <input type="radio" name="size-options" id="L" value="large" className="multiple-choice-option"/>
+              <label htmlFor='L'>L</label>
+            </>
           </div>}
           {isSingleSize && <div className="single-choice-container">
             <input type="radio" name="size-options" id="onesize" value="onesize" className="single-choice-option"/>
