0ff183057a70815029a1f2fec839fbdd424859b4
Taranveer Singh <taranveersingh93@gmail.com>
2023-08-31 16:29:32 -0400

    Refactor tests to be dynamic

--- cypress/e2e/home_spec.cy.js
+++ cypress/e2e/home_spec.cy.js
@@ -3,12 +3,15 @@
 /* eslint-disable no-undef */
 describe('home page', () => {
   beforeEach(() => {
+    cy.intercept('POST', 'https://everuse-be-b6017dbfcc94.herokuapp.com/graphql', (req) => {
+      req.alias = req.body.operationName;
+      req.reply({
+        statusCode: 201,
+        fixture: `${req.body.operationName}GQL`
+      })
+    })
     cy.visit('http://localhost:3000/')
-    cy.intercept('POST', 'https://everuse-be-b6017dbfcc94.herokuapp.com/graphql', {
-      statusCode: 201,
-      fixture: 'items.json'
-    }).as('getItems')
-    cy.wait('@getItems')
+    cy.wait('@GetAllItems')
   })
   
   it('should display tagline and button on landing page', () => {
