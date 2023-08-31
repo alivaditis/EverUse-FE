e3434df9357dc3ea1c023911d62d62d7b169518a
Alec Livaditis <aleclivaditis@gmail.com>
2023-08-31 18:24:02 -0400

    resolve merge conflict

--- cypress/e2e/checkout_spec.cy.js
+++ cypress/e2e/checkout_spec.cy.js
@@ -1,3 +1,33 @@
+const initialShoppingBag = [
+  {
+    id: 1,  
+    type: "bracelet",
+    color: "moss",
+    size: 'M',
+    quantity: 2,
+    price: 10.00,
+    image: 'https://cdn.shopify.com/s/files/1/0192/8012/products/friendship-bracelet-adjustable-camp-minimalist-rope-dowling-brothers-bangle-jewellery-740.jpg'
+  },
+  {
+    id: 2,
+    type: "bracelet",
+    color: "orange plaid",
+    size: 'S',
+    quantity: 3,
+    price: 10.00,
+    image: 'https://cdn.shopify.com/s/files/1/0192/8012/products/friendship-bracelet-adjustable-camp-minimalist-rope-dowling-brothers-bangle-jewellery-740.jpg'
+  },
+  {
+    id: 3,
+    type: "leash",
+    color: "lime",
+    size: 'onesize',
+    quantity: 1,
+    price: 20.00,
+    image: 'https://cdn.shopify.com/s/files/1/0192/8012/products/friendship-bracelet-adjustable-camp-minimalist-rope-dowling-brothers-bangle-jewellery-740.jpg'
+  }
+]
+
 describe('checkout', () => {
   beforeEach(() => {
     cy.intercept('POST', 'https://everuse-be-b6017dbfcc94.herokuapp.com/graphql', {
