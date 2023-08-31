2058255c78e8c985e91ef4ab7d9156a4250ac717
Alec Livaditis <aleclivaditis@gmail.com>
2023-08-31 17:21:55 -0400

    fix initial cookies issue

--- src/components/App.js
+++ src/components/App.js
@@ -17,15 +17,23 @@ import { cleanFetchedData } from '../helperFunctions';
 
 function App() {
   const [cookies, setCookie] = useCookies(['shoppingBag']);
-  const [shoppingBag, setShoppingBag] = useState(
-      [...cookies.shoppingBag]   
-    )
+  const [shoppingBag, setShoppingBag] = useState([])
     
   const { loading, error, data } = useQuery(GET_ALL_ITEMS)
   const [totalPrice, setTotalPrice] = useState(0);
   const [items, setItems] = useState([])
   const [itemsForDisplay, setItemsForDisplay] = useState([])
   const [successMessage, setSuccessMessage] = useState('')
+
+  useEffect(() => {
+    if (cookies.shoppingBag) {
+      setShoppingBag([...cookies.shoppingBag]);
+    }
+  }, [])  
+
+  useEffect(() => {
+    setCookie('shoppingBag', shoppingBag)
+  }, [shoppingBag])
 
   const addTotalPrice = () => {
     const total = shoppingBag.reduce((price, item) => {
@@ -51,9 +59,6 @@ function App() {
     addTotalPrice();
   }, [shoppingBag])
 
-  useEffect(() => {
-    setCookie('shoppingBag', shoppingBag)
-  }, [shoppingBag])
 
   const removeItemFromBag = id => {
     setShoppingBag(shoppingBag.filter(item => item.id !== parseInt(id)))
