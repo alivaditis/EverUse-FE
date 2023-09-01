48812109ad72ad0f4999f2b91f2173a3937cf42a
Rachel Prather <rachelprather1@gmail.com>
2023-09-01 13:15:11 -0700

    Import Footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import logo from '../images/logo-white.png'
 import instagram from '../images/1024px-Instagram_icon.png'
+import '../styles/_Footer.scss'
 
 const Footer = () => {
   return ( 
     <div className='footer'>
-      <img src={logo} alt="EverUse logo in orange, curvy text" className='footer__logo' /> 
+      <img src={logo} alt="EverUse logo in white, curvy text" className='footer__logo' /> 
       <p className='footer__text'>© 2023 EverUse Upcycled Products</p>
       <a href='https://www.instagram.com/everuseproducts/?hl=en'><img className='footer__instagram' src={instagram} alt='instagram '/></a>
       
