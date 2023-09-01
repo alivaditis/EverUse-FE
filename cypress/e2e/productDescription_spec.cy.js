3c224b16fe07d46c57140d0baf252a3d7dfcbd57
Taranveer Singh <taranveersingh93@gmail.com>
2023-09-01 01:41:55 -0400

    Make some element tests

--- cypress/e2e/productDescription_spec.cy.js
+++ cypress/e2e/productDescription_spec.cy.js
@@ -5,9 +5,13 @@ describe('home page', () => {
     cy.wait('@GetItem');
   })
 
-  it('should do this', () => {
+  it('should locate the right elements', () => {
     cy.viewport(850,1000);
-    cy.get('.completed').should('have.css', 'text-decoration', 'line-through')
-    cy.get('.completed').should('have.css', 'color', 'rgb(217,217,217)')
+    cy.get('.details__header').should('have.css', 'background-image', 'url("http://localhost:3000/static/media/detail-horizontal-bg.4ba54468bd483e6c6dc5.jpg")')
+    cy.get('.details__header-text').contains('Products handmade from upcycled climbing ropes in an effort to reduce waste')
+    cy.get('.product-description-text').contains('EverUse collects and upcycles used climbing rope into high-quality crafts and products. As two fellow rock climbers, we are passionate about both the sport and planet.')
+    cy.get('.details-order-form__title').find('p').eq(0).contains('Beer Koozie')
+    cy.get('.multiple-choice-container').find('label').should('have.length', 3)
+    
   })
 })
