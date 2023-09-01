df84c245f0e90ccfa3a31bf0f9d54bf274137f57
Rachel Prather <rachelprather1@gmail.com>
2023-08-31 17:44:13 -0700

    Add logo and email to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
+import logo from '../images/everuse-logo.png'
+
 const Footer = () => {
   return ( 
     <div className='footer'>
-      stuff
+      <img src={logo} alt="EverUse logo in orange, curvy text" className='footer__logo' /> 
+      <p className='footer__text'>contact@everuseproducts.com</p>
+      
     </div>
   )
 }
