b4bc90bd3aab2286a6ab4bb01f46cd94a8e8cd42
Alec Livaditis <aleclivaditis@gmail.com>
2023-09-01 13:15:12 -0400

    add instagram photo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import logo from '../images/everuse-logo.png'
+import instagram from '../images/1024px-Instagram_icon.png'
+
 
 const Footer = () => {
   return ( 
     <div className='footer'>
       <img src={logo} alt="EverUse logo in orange, curvy text" className='footer__logo' /> 
-      <p className='footer__text'>contact@everuseproducts.com</p>
+      <p className='footer__text'>© 2023 EverUse Upcycled Products</p>
+      <a href='https://www.instagram.com/everuseproducts/?hl=en'><img className='footer__instagram' src={instagram} alt='instagram '/></a>
       
     </div>
   )
