bf2cd54a9bc4d426d3b3767fc9b88edcb9b78cc9
Jamie Caudill <j.caudill7177@gmail.com>
2023-09-03 13:09:56 -0700

    Clean up

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,4 +1,4 @@
-const camelToPascalCase = path => {
+const camelToPascalCase = (path) => {
   const characters = path.split("");
   const newCharacters = [];
   for (let i = 0; i < characters.length; i++) {
@@ -12,38 +12,41 @@ const camelToPascalCase = path => {
     }
   }
   return newCharacters.join("");
-}
+};
 
 const cleanFetchedData = (items) => {
-  const productNames = new Set (items.map(product => product.name));
+  const productNames = new Set(items.map((product) => product.name));
   const productsForDisplay = [];
 
-  productNames.forEach(productName => {
-      
-    const filteredProducts = items
-      .filter(product => product.name === productName)
-    
-    const filteredProduct = filteredProducts.reduce((acc, curr) => {
-      acc.id = curr.name;
-      acc.name = curr.name;
-      acc.price = curr.price;
-      acc.category = curr.category
-      acc.image = curr.image
-      acc.description = curr.description
-      if (!acc.colorOptions.includes(curr.color)) {
-        acc.colorOptions.push(curr.color)
+  productNames.forEach((productName) => {
+    const filteredProducts = items.filter(
+      (product) => product.name === productName
+    );
+
+    const filteredProduct = filteredProducts.reduce(
+      (acc, curr) => {
+        acc.id = curr.name;
+        acc.name = curr.name;
+        acc.price = curr.price;
+        acc.category = curr.category;
+        acc.image = curr.image;
+        acc.description = curr.description;
+        if (!acc.colorOptions.includes(curr.color)) {
+          acc.colorOptions.push(curr.color);
+        }
+        if (!acc.sizeOptions.includes(curr.size)) {
+          acc.sizeOptions.push(curr.size);
+        }
+        return acc;
+      },
+      {
+        colorOptions: [],
+        sizeOptions: [],
       }
-      if (!acc.sizeOptions.includes(curr.size)) {
-        acc.sizeOptions.push(curr.size)
-      }
-      return acc;
-    }, {
-      colorOptions: [],
-      sizeOptions: []
-    })
-    productsForDisplay.push(filteredProduct)
-  }) 
-  return productsForDisplay
-}
+    );
+    productsForDisplay.push(filteredProduct);
+  });
+  return productsForDisplay;
+};
 
 export { camelToPascalCase, cleanFetchedData };
